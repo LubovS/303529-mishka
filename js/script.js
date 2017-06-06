a4faefd47a1586fabec857f92637eab0cf429886
Lubov Sagitova <lubovtulaeva@gmail.com>
2017-06-06 23:09:28 +0300

    Убраны абсолюты и добавлено модально окно на js на главной

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggler');
+var popup = document.querySelector('.pop-up-order');
+var btnpurchase = document.querySelector('.product-week__btn-purchase');
+var overlay = document.querySelector('.overlay');
+var products = document.querySelector('.products__btn-purchase');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+       
+btnpurchase.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.add('pop-up-order--open');
+  overlay.classList.add("overlay--show");
+});
+
+overlay.addEventListener('click', function() {
+  popup.classList.remove("pop-up-order--open"), overlay.classList.remove("overlay--show")
+});
+
+window.addEventListener("keydown", function() {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("pop-up-order--open")) {
+      popup.classList.remove("pop-up-order--open"), overlay.classList.remove("overlay--show");
+    }
+  }
+});
