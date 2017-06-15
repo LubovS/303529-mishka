1e0ce999c3305c743da9066d66815b19022ba3c3
Lubov Sagitova <lubovtulaeva@gmail.com>
2017-06-15 17:00:02 +0300

    Испралены расползающие блоки, и добавлен js в каталог

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var navToggle = document.querySelector('.main-nav__toggler');
 var popup = document.querySelector('.pop-up-order');
 var btnpurchase = document.querySelector('.product-week__btn-purchase');
 var overlay = document.querySelector('.overlay');
-var products = document.querySelector('.products__btn-purchase');
+var products = document.querySelectorAll('.products__btn-purchase');
+
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -14,6 +15,26 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+for (var i = 0; i < products.length; i++) {
+  products[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    popup.classList.add('pop-up-order--open');
+    overlay.classList.add("overlay--show");
+  });
+}
+
+overlay.addEventListener('click', function() {
+  popup.classList.remove("pop-up-order--open"), overlay.classList.remove("overlay--show")
+});
+
+window.addEventListener("keydown", function() {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("pop-up-order--open")) {
+      popup.classList.remove("pop-up-order--open"), overlay.classList.remove("overlay--show");
+    }
   }
 });
 
